f0681821d516b5e26d5ac7831ea90d673792a1d7
Victor Lara <166436119+VictorHLara@users.noreply.github.com>
2025-01-05 23:02:07 -0300

    Add files via upload

---
+++ criacao.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE 8107_;
+
+
+
+CREATE TABLE Abrigo (
+
+);
+
+CREATE TABLE Adocao (
+
+);
+
+CREATE TABLE Adotante (
+
+);
+
+CREATE TABLE Animal (
+
+);
+
+CREATE TABLE Especie (
+
+);
