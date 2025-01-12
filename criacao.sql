2e21b32c5b169b0a7c22e1dc32e4774cc655dad1
Victor Lara <166436119+VictorHLara@users.noreply.github.com>
2025-01-11 23:16:00 -0300

    Update criacao.sql

--- criacao.sql
+++ criacao.sql
@@ -1,88 +1,100 @@
 CREATE DATABASE 8107_8116_8123_8748_8772;
 
 
+-- Tabela Abrigo
 CREATE TABLE Abrigo (
-	CNPJ INT PRIMARY KEY NOT NULL,
-	Nome_Abrigo VARCHAR(45) NOT NULL,
-	Endereco_Abrigo VARCHAR(45) NOT NULL,
-	CPF_Gerente CHAR(11) NOT NULL,
-	Email_Abrigo VARCHAR(45) NOT NULL,
+    CNPJ INT PRIMARY KEY NOT NULL,
+    Nome_Abrigo VARCHAR(45) NOT NULL,
+    Endereco_Abrigo VARCHAR(45) NOT NULL,
+    CPF_Gerente CHAR(11) NOT NULL,
+    Email_Abrigo VARCHAR(45) NOT NULL
 );
 
+-- Tabela Adocao
 CREATE TABLE Adocao (
-	ID_Adocao INT PRIMARY KEY NOT NULL,
-	Data_Adocao DATE NOT NULL,
+    ID_Adocao INT PRIMARY KEY NOT NULL,
+    Data_Adocao DATE NOT NULL,
+    CNPJ_Abrigo INT NOT NULL, -- FK para Abrigo
+    CPF_Adotante CHAR(11) NOT NULL, -- FK para Adotante
+    ID_Animal INT NOT NULL -- FK para Animal
 );
 
+-- Tabela Adotante
 CREATE TABLE Adotante (
-	CPF CHAR(11) PRIMARY KEY NOT NULL,
-	Nome_Adotante VARCHAR(45) NOT NULL,
-	Email_Adotante VARCHAR(45) NOT NULL,
-	Data_Nascimento DATE NOT NULL,
-	Endereco_Adotante VARCHAR(45) NOT NULL,
+    CPF CHAR(11) PRIMARY KEY NOT NULL,
+    Nome_Adotante VARCHAR(45) NOT NULL,
+    Email_Adotante VARCHAR(45) NOT NULL,
+    Data_Nascimento DATE NOT NULL,
+    Endereco_Adotante VARCHAR(45) NOT NULL
 );
 
+-- Tabela Animal
 CREATE TABLE Animal (
-	ID_Animal INT PRIMARY KEY NOT NULL,
-	Nome_Animal VARCHAR(45) NOT NULL,
-	Vacinas VARCHAR(45) NOT NULL,
-	Descricao VARCHAR(200) NOT NULL,
-	Sexo VARCHAR(45) NOT NULL,
-	Idade INT NOT NULL,
+    ID_Animal INT PRIMARY KEY NOT NULL,
+    Nome_Animal VARCHAR(45) NOT NULL,
+    Vacinas VARCHAR(45) NOT NULL,
+    Descricao VARCHAR(200) NOT NULL,
+    Sexo VARCHAR(45) NOT NULL,
+    Idade INT NOT NULL,
+    Raca VARCHAR(25) NOT NULL,
+    CNPJ_Abrigo INT NOT NULL, -- FK para Abrigo
+    Cod_Especie INT NOT NULL -- FK para Especie
 );
 
+-- Tabela Especie
 CREATE TABLE Especie (
-	Cod_Especie INT PRIMARY KEY NOT NULL,
-	Cuidados VARCHAR(1000),
-	Raca VARCHAR(25),
+    Cod_Especie INT PRIMARY KEY NOT NULL,
+    Cuidados VARCHAR(1000) NOT NULL
 );
 
+-- Tabela Telefone_Abrigo
 CREATE TABLE Telefone_Abrigo (
-	Telefone_Abrigo VARCHAR(45) NOT NULL,
+    Telefone_Abrigo VARCHAR(45) NOT NULL,
+    CNPJ_Abrigo INT NOT NULL -- FK para Abrigo
 );
 
+-- Tabela Telefone_Adotante
 CREATE TABLE Telefone_Adotante (
-	Telefone_Adotante VARCHAR(45) NOT NULL,
+    Telefone_Adotante VARCHAR(45) NOT NULL,
+    CPF_Adotante CHAR(11) NOT NULL -- FK para Adotante
 );
 
+-- Tabela Fotos
 CREATE TABLE Fotos (
-	Fotos_Animal MEDIUMBLOB NOT NULL
+    ID_Foto INT PRIMARY KEY AUTO_INCREMENT,
+    Fotos_Animal MEDIUMBLOB NOT NULL,
+    ID_Animal INT NOT NULL -- FK para Animal
 );
 
--- chave estrangeira entre Adocao e Abrigo
+-- Adicionar as chaves estrangeiras
 ALTER TABLE Adocao ADD CONSTRAINT FK_Adocao_Abrigo
-    FOREIGN KEY (CNPJ_Abrigo)
-    REFERENCES Abrigo (CNPJ);
+    FOREIGN KEY (CNPJ_Abrigo) REFERENCES Abrigo (CNPJ);
 
--- chave estrangeira entre Adocao e Adotante
 ALTER TABLE Adocao ADD CONSTRAINT FK_Adocao_Adotante
-    FOREIGN KEY (CPF_Adotante)
-    REFERENCES Adotante (CPF);
+    FOREIGN KEY (CPF_Adotante) REFERENCES Adotante (CPF);
 
--- chave estrangeira entre Adocao e Animal
+
 ALTER TABLE Adocao ADD CONSTRAINT FK_Adocao_Animal
-    FOREIGN KEY (ID_Animal)
-    REFERENCES Animal (ID_Animal);
+    FOREIGN KEY (ID_Animal) REFERENCES Animal (ID_Animal);
 
--- chave estrangeira entre Animal e Abrigo
+
 ALTER TABLE Animal ADD CONSTRAINT FK_Animal_Abrigo
-    FOREIGN KEY (CNPJ_Abrigo)
-    REFERENCES Abrigo (CNPJ);
+    FOREIGN KEY (CNPJ_Abrigo) REFERENCES Abrigo (CNPJ);
 
--- chave estrangeira entre Telefone_Abrigo e Abrigo
+ALTER TABLE Animal ADD CONSTRAINT FK_Animal_Especie
+	FOREIGN KEY (Cod_Especie) REFERENCES Especie (Cod_Especie);
+
+
 ALTER TABLE Telefone_Abrigo ADD CONSTRAINT FK_Telefone_Abrigo
-    FOREIGN KEY (CNPJ_Abrigo)
-    REFERENCES Abrigo (CNPJ);
+    FOREIGN KEY (CNPJ_Abrigo) REFERENCES Abrigo (CNPJ);
 
--- chave estrangeira entre Telefone_Adotante e Adotante
+
 ALTER TABLE Telefone_Adotante ADD CONSTRAINT FK_Telefone_Adotante
-    FOREIGN KEY (CPF_Adotante)
-    REFERENCES Adotante (CPF);
+    FOREIGN KEY (CPF_Adotante) REFERENCES Adotante (CPF);
 
--- chave estrangeira entre Fotos e Animal
+
 ALTER TABLE Fotos ADD CONSTRAINT FK_Foto_Animal
-    FOREIGN KEY (ID_Animal)
-    REFERENCES Animal (ID_Animal);
+    FOREIGN KEY (ID_Animal) REFERENCES Animal (ID_Animal);
 
 
 
